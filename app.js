127fbd512cd8132dcaa93b9eb908dce2714200c2
Justin Passmore <justinwpassmore@gmail.com>
2022-06-28 13:54:51 -0600

    Completed exercise

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log("Let's get this party started!");
+const $gifContainer = $("#gif-container");
+const $searchInput = $("#user-input");
+
+$("form").on("submit", async function(e){
+    e.preventDefault();
+
+    let searchValue = $searchInput.val();
+    $searchInput.val("");
+
+    const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
+    params: {
+      q: searchValue,
+      api_key: "x8DukhUTVsGt5EXV5DuWMtZhtmaRxghk"
+    }
+  });
+  addGif(response.data);
+});
+
+function addGif(res) {
+    let numResults = res.data.length;
+    if (numResults) {
+      let randomIdx = Math.floor(Math.random() * numResults);
+      let $newCol = $("<div>", { class: "col-md-4 col-12 mb-4" });
+      let $newGif = $("<img>", {
+        src: res.data[randomIdx].images.original.url,
+        class: "w-100"
+      });
+      $newCol.append($newGif);
+      $gifContainer.append($newCol);
+    }
+  }
+
+  $("#remove").on("click", function() {
+    $gifArea.empty();
+  });
